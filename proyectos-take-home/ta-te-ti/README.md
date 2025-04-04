a45d4ffd01d3a56cf9a3e7fd3ece98551be861b9
Luxorion <benjaminbeccari.1@gmail.com>
2025-04-04 15:40:17 -0300

    Resolviendo TakeAHome Tateti

--- proyectos-take-home/ta-te-ti/README.md
+++ proyectos-take-home/ta-te-ti/README.md
@@ -2,7 +2,7 @@
 Debemos implementar un juego de Ta-Te-Ti (o proyectos-take-home/ta-te-ti).
 
 ## Requisitos
-- Clickear las celdas vacías deberían agregar la ficha del turno activo.
+-X Clickear las celdas vacías deberían agregar la ficha del turno activo.
 - Al ganar o empatar no deberíamos poder seguir agregando fichas nuevas.
 - Al terminar deberiamos poder reiniciar la partida para volver a jugar.
 
--- proyectos-take-home/ta-te-ti/src/App.tsx
+++ proyectos-take-home/ta-te-ti/src/App.tsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
+
 const GRID = Array.from(Array(9).keys());
 
+type Persona = 0 | 1;
+type Board = (string | null)[];
+
 function App() {
+  const [player, setPlayer] = useState<Persona>(0);
+  const [board, setBoard] = useState<Board>(Array(9).fill(null));
+
+  const handleClick = (indice: number) => {
+    if (board[indice]) return;
+
+    const newBoard = [...board];
+
+    newBoard[indice] = player === 0 ? "X" : "O";
+    setBoard(newBoard);
+    setPlayer(player === 0 ? 1 : 0);
+  };
+
   return (
     <main>
       {GRID.map((i) => (
-        <div key={i} className="square">
-          {""}
+        <div key={i} className="square" onClick={() => handleClick(i)}>
+          {board[i]}
         </div>
       ))}
     </main>
